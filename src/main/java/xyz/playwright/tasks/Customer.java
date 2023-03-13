a2c991e78f4d9f741e62355b40f09c7b6fc483c2
Donatas Barkauskas <donatas.barkauskas@sage.com>
2023-03-13 17:06:23 +0200

    Customer can Deposit money to his Account

--- src/main/java/xyz/playwright/tasks/Customer.java
+++ src/main/java/xyz/playwright/tasks/Customer.java
@@ -3,10 +3,29 @@ package xyz.playwright.tasks;
 import com.microsoft.playwright.Page;
 import xyz.playwright.model.AccountSummary;
 import xyz.playwright.userInterface.CustomerHomePage;
+import xyz.playwright.userInterface.CustomerTransactionPage;
 
 public class Customer {
     public static AccountSummary getSummary(Page page) {
         CustomerHomePage customerHomePage = new CustomerHomePage(page);
         return new AccountSummary(customerHomePage.textAccountSummary.innerText());
+    }
+
+    public static void selectAccount(Page page, String accountNumber) {
+        CustomerHomePage customerHomePage = new CustomerHomePage(page);
+        customerHomePage.drpAccounts.selectOption(accountNumber);
+    }
+
+    public static void deposit(Page page, Integer amount) {
+        CustomerHomePage customerHomePage = new CustomerHomePage(page);
+        customerHomePage.buttonDeposit.click();
+        CustomerTransactionPage customerTransactionPage = new CustomerTransactionPage(page);
+        customerTransactionPage.inputAmount.fill(amount.toString());
+        customerTransactionPage.buttonTransaction.click();
+    }
+
+    public static String getSuccessMessage(Page page) {
+        CustomerHomePage customerHomePage = new CustomerHomePage(page);
+        return customerHomePage.labelMessage.textContent();
     }
 }
